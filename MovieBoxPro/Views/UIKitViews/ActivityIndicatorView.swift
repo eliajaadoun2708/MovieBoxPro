08ec8746e2a95070a0f5fb74582160dbc505ab50
elia jadoun <elia.jadoun11@gmail.com>
2021-03-13 19:34:14 +0200

    hi

---
+++ MovieBoxPro/Views/UIKitViews/ActivityIndicatorView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityIndicatorView.swift
+//  MovieBoxPro
+//
+//  Created by elia jadoun on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ActivityIndicatorView: UIViewRepresentable {
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let view = UIActivityIndicatorView(style: .large)
+        view.startAnimating()
+        return view
+    }
+}
+
